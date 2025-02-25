e72f10cdf85dc3a4471cf8c859e01e998333d1e9
carlosble <carlos@leanmind.es>
2025-02-25 18:43:11 +0000

    initial version of the tests

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# quick-tdd-intro-ts
-Quick intro to Test-Driven Development with TypeScript
+# Quick TDD intro using TypeScript
+
+Quick workshop to get started with TDD and
+simple design using TypeScript. 
+
+## Instructions
+
+There is no implementation code left in the main branch, just the tests are there. However, tests are ignored (.skip). You will have to create a file Hangman.ts and implement the minimun and simplest code to make each test pass. One by one.
+
+You probably want to disable Copilot or any other AI plugin to be able to practise TDD. Otherwise, given the clarity of the test names and the expresivity of the API, the AI will generate all the implementation in a single shot. 
+
+* Unignore/enable the first test. Run the test to see it fail. (remove .skip from it) 
+* Write the minium implementation the fastest one to make it green. 
+* Run the test to see it pass.
+* See if you want to refactoring a bit.
+* Go for the next test and keep going. 
+
+
+## Setup
+
+Node v22 is required to run this project.
+
+> 💡If you don't have it installed, we recommend the use of a version manager
+> like [_nvm_](https://github.com/nvm-sh/nvm) or [_fnm_](https://github.com/Schniz/fnm) (a cross-platform alternative to
+> _nvm_). After installing the manager, you can install the Node version by running: `nvm use` or `fnm use`.
+
+Once you have Node installed, you can install the project dependencies by running:
+
+`npm install`
+
+### Plugins
+
+This project uses the following plugins:
+
+- [Vitest](https://vitest.dev/), a blazing fast test framework.
+
+> ℹ️ If VSCode is your editor of choice, you can install the recommended extensions.
+
+## Run tests
+
+You can run the tests by running:
+
+`npm run test`
+
+If you want to run the tests in watch mode, you can run:
+
+`npm run test:watch`
+
+Also, you can run the tests with coverage by running:
+
+`npm run test:coverage`
---
+++ src/Hangmang.test.ts
@@ -0,0 +1,101 @@
+
+import { describe, it, expect} from "vitest";
+import { GameError, GameResult, Guess, Hangmang, GameError as Misconfiguration } from "./Hangman";
+
+
+describe("Hangmang machine", () => {
+
+    function startGame(secretWord: string, trials: number) {
+        return Hangmang.startGame({secretWord, trials})
+    }
+
+    it.skip("finishes the game when all trials are consumed", () => {
+        let game = startGame("p", 1)
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.isOver()).toBe(true)
+    })
+
+    it.skip("finishes the game when the player guesses the secret word", () => {
+        let game = startGame("p", 5)
+        game = game.tryTo(Guess.letter('p'))
+        expect(game.isOver()).toBe(true)
+    })
+
+    it.skip("knows when the player wins", () => {
+        let game = startGame("p", 5)
+        game = game.tryTo(Guess.letter('p'))
+        expect(game.result()).toEqual(GameResult.PlayerWins)
+    })
+
+    it.skip("knows when the player loses", () => {   
+        let game = startGame("p", 1)
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.result()).toEqual(GameResult.PlayerLoses)   
+    })
+
+    it.skip("knows that the game is ongoing", () => {
+        let game = startGame("p", 5)
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.result()).toEqual(GameResult.Ongoing)
+        expect(game.isOver()).toBe(false)
+    })
+
+    it.skip("counts the number of trials available", () => {
+        let game = startGame("p", 5)
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.availableTrials()).toEqual(4)
+    })
+
+    it.skip("reveals only the part of the secret that has been guessed", () => {
+        let game = startGame("cat", 5)
+        expect(game.revealedSecret()).toEqual("___")
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.revealedSecret()).toEqual("_a_")
+    })
+
+    it.skip("can't play a game with wrong arguments", () => {
+        let game = startGame("cat", -7)
+        expect(game.isOver()).toBe(true)
+        expect(game.isMisconfigured()).toBe(true)
+        expect(game.problem()).toBe(Misconfiguration.TrialsMustBePositive)
+        expect(game.revealedSecret()).toEqual("___")
+        expect(game.availableTrials()).toEqual(0)
+
+        game = startGame("f", 15)
+        expect(game.isOver()).toBe(true)
+        expect(game.problem()).toBe(Misconfiguration.SecretWordMustHaveThreeLetters)
+    })
+
+    it.skip("does not alter a game that is already over", () => {
+        let game = startGame("cat", 1)
+        game = game.tryTo(Guess.letter('a'))
+        game = game.tryTo(Guess.letter('b'))
+        game = game.tryTo(Guess.letter('c'))
+        expect(game.isOver()).toBe(true)
+        expect(game.availableTrials()).toEqual(0)
+    })
+
+    it.skip("does not allow for words or multiple letters when guessing", () => {
+        let game = startGame("cat", 5)
+        game = game.tryTo(Guess.letter('ca'))
+        expect(game.revealedSecret()).toEqual("___")
+        expect(game.problem()).toBe(GameError.MultipleLettersNotAllowed)
+        expect(game.availableTrials()).toEqual(5)
+    })
+
+    it.skip("does not allow for symbols or numbers, just letters a to z", () => {
+        let game = startGame("cat", 5)
+        game = game.tryTo(Guess.letter('1'))
+        expect(game.revealedSecret()).toEqual("___")
+        expect(game.problem()).toBe(GameError.InvalidCharacter)
+        expect(game.availableTrials()).toEqual(5)
+    })
+
+    it.skip("allows for the game to continue after an invalid trial", () => {
+        let game = startGame("cat", 5)
+        game = game.tryTo(Guess.letter('1'))
+        game = game.tryTo(Guess.letter('a'))
+        expect(game.revealedSecret()).toEqual("_a_")
+        expect(game.availableTrials()).toEqual(4)
+    })
+})
