5a057183f8db94b2c0b9c872f1907b01e9a65f0b
Isidro Lopez <islomar@gmail.com>
2025-03-30 21:47:21 +0200

    Remove unused field 'trials'

--- src/Hangman.ts
+++ src/Hangman.ts
@@ -1,12 +1,10 @@
 export class Game {
     private readonly secretWord: string;
-    private trials: number;
     private remainingTrials: number;
     private remainingLettersToGuess: string;
 
     constructor(config: {secretWord: string, trials: number }) {
         this.secretWord = config.secretWord
-        this.trials = config.trials
         this.remainingLettersToGuess = [...new Set(this.secretWord)].toString();
         this.remainingTrials = config.trials
     }
